9602cf18cd2792078c0a7043cc3bcad1de15341a
Rohit Raj <rohit.raj@aalto.fi>
2019-09-28 19:25:56 +0300

    Adding script for installing docker and mongodb image

---
+++ assignment_1_801636/code/scripts/install_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+#Installation on ubuntu
+
+sudo apt-get update
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
