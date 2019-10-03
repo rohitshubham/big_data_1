1db6e59c2ff7a4dcdf046628347ddb7561df1118
Rohit Raj <rohit.raj@aalto.fi>
2019-10-04 01:28:48 +0300

    Enable sharding in mongodb using docker-compose plus rearrange the logs and add new logs

--- assignment_1_801636/code/scripts/install_mongo_container.sh
+++ assignment_1_801636/code/scripts/install_mongo_container.sh
@@ -1,12 +1,19 @@
+## Builds, Installs a 3 shard MongoDB cluster with a replica of 2 for each shard
+## There are 3 config servers for the metadata and this script automatically enables sharding for a database named mysimbdp-coredms
 #!/bin/sh
 set -e
 
 #pulling the mongodb docker image
 sudo docker pull mongo
 
-#run the image
-# map the port and mount the volume to /usr/share directory
-sudo docker run --name my-mongo -d -v /usr/share/mongodb:/data/db -p 27017:27017 mongo
+#run the docker-compose image
+sudo docker-compose up -f ../mysimbdp-coredms/mongo_sharded_docker/docker-compose.yml -d
+
+#run the first init.sh script
+sudo bash ../mysimbdp-coredms/mongo_sharded_docker/init.sh
 
 # check if the instance has been started
 sudo docker ps
+
+#enable sharding on the database name
+sudo docker-compose exec router mongo --eval "sh.enableSharding('mysimbdp-coredms')"
